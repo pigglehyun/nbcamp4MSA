6ef4410da10a617b0d1d97c24460e189f7cc3839
pigglehyun <sarah0661@pusan.ac.kr>
2025-07-31 22:41:23 +0900

    Feat(Auth): User Data

---
+++ com.spring-cloud.eureka.auth/src/main/java/com/spring_cloud/eureka/auth/User.java
@@ -0,0 +1,29 @@
+package com.spring_cloud.eureka.auth;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "user_tb")
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+}
