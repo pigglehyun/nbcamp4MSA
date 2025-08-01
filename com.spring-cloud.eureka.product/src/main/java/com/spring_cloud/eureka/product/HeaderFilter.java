dc5922c06c43c6966f2f68abd5ddae3f994050de
pigglehyun <sarah0661@pusan.ac.kr>
2025-08-01 15:09:02 +0900

    Feat(Product): add Port num to Header

---
+++ com.spring-cloud.eureka.product/src/main/java/com/spring_cloud/eureka/product/HeaderFilter.java
@@ -0,0 +1,17 @@
+package com.spring_cloud.eureka.product;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class HeaderFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        int serverPort = request.getServerPort();
+        ((HttpServletResponse) response).setHeader("Server-Port", String.valueOf(serverPort));
+        chain.doFilter(request, response);
+    }
+}
